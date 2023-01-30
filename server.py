63a9b04884793f2f6083127fe161b5f9af14530c
yunicoder <t.yabe.240@ms.saitama-u.ac.jp>
2023-01-30 10:58:39 +0900

    change to no timeout and recieve all port

--- server.py
+++ server.py
@@ -6,7 +6,7 @@ from utils.server_base import BaseServer
 class InetServer(BaseServer):
     def __init__(self, host: str = "0.0.0.0", port: int = 8080) -> None:
         self.address = (host, port)
-        super().__init__(timeout=60, buffer=1024)  # 親クラスのinit
+        super().__init__(timeout=600000, buffer=1024)  # 親クラスのinit
         self.accept(self.address, socket.AF_INET, socket.SOCK_STREAM, 0)
 
     def start_server(self):
@@ -21,6 +21,5 @@ class InetServer(BaseServer):
 
 
 if __name__ == "__main__":
-    server = InetServer(host="127.0.0.1", port=8080)
-    # server = InetServer(host="192.168.7.95", port=8080)  # for coolidge
+    server = InetServer(host="0.0.0.0", port=8080)
     server.start_server()
